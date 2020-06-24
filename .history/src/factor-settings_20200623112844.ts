d804f15fb20982da7829c62843eb2ad761fbf9a9
Vincent Hirtz <vincent.hirtz@smartpanda.com>
2020-06-24 11:12:25 +0200

    init site

--- .history/src/factor-settings_20200623112844.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Factor Settings - How settings work in Factor...
- *
- * Your settings files are how you customize themes and plugins.
- * They allow you to set text and images, but also override components, routes and more.
- *
- */
-export default {}
--- .history/src/index_20200623145340.ts
+++
@@ -1,5 +0,0 @@
-/**
- * This is your app main file
- *
- * Use it to do anything you want in your Factor app
- */
